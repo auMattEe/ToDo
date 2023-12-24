4479ca0b2f93730cd8a777825f66d62e94d470f4
auMattEe <matt.eaton85@gmail.com>
2023-12-24 17:17:11 +0200

    Todo.java updated.

--- src/main/java/entity/Todo.java
+++ src/main/java/entity/Todo.java
@@ -1,4 +1,36 @@
 package entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "todo")
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "completed", nullable = false)
+    private boolean completed;
+
+    @Column(name = "date_created", nullable = false)
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated", nullable = false)
+    @UpdateTimestamp
+    private Date lastUpdated;
 }
