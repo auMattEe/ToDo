7ce598cc181b09ba6dd199d8cfee260c24c53ce1
auMattEe <matt.eaton85@gmail.com>
2023-12-24 18:14:55 +0200

    Developed TodoService.

--- src/main/java/com/todoapp/backend/service/TodoService.java
+++ src/main/java/com/todoapp/backend/service/TodoService.java
@@ -1,4 +1,15 @@
 package com.todoapp.backend.service;
 
+import com.todoapp.backend.repo.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 }
